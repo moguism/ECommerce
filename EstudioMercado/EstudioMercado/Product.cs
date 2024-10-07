08a71f1c7fab07d0d6d68e2fb55ce9cef71a3b39
moguism <148863935+moguism@users.noreply.github.com>
2024-10-07 20:20:42 +0200

    Agregado web scrapping a Dia
    Quizás haya que cambiar algo

---
+++ EstudioMercado/EstudioMercado/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace EstudioMercado;
+
+internal class Product
+{
+    // EL "init" ESPECIFICA QUE SÓLO SE PUEDE ESCRIBIR UNA VEZ, AL INICIO DE LA CLASE EN EL CONSTRUCTOR A
+    public string Name { get; init; }
+    public decimal Price { get; init; }
+    public Product(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder(); // Para convertir a cadenas de texto de forma eficiente
+        stringBuilder.AppendLine($"Name: {Name}");
+        stringBuilder.AppendLine($"Price: {Price} euros");
+        return stringBuilder.ToString();
+
+    }
+}
