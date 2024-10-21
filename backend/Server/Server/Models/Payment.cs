5cf04978a74702338a6d305963e036721db36f0f
moguism <148863935+moguism@users.noreply.github.com>
2024-10-21 17:55:11 +0200

    Agregado servidor, con modelos ya listos
    Cambio en la estructura de carpetas

---
+++ backend/Server/Server/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server.Models;
+
+public partial class Payment
+{
+    public DateTime CreatedAt { get; set; }
+
+    public int Id { get; set; }
+
+    public double Total { get; set; }
+
+    public int IsDone { get; set; }
+
+    public int OrderId { get; set; }
+
+    public int PaymentTypeId { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+
+    public virtual PaymentsType PaymentType { get; set; } = null!;
+}
---
+++ backend/Server/Server/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server.Models;
+
+public partial class Review
+{
+    public int Id { get; set; }
+
+    public string Text { get; set; } = null!;
+
+    public int Score { get; set; }
+
+    public int UserId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
