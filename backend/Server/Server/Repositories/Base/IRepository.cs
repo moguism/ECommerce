7bd2924ccfa1e879743af8ca441d87cb1c38b37d
moguism <148863935+moguism@users.noreply.github.com>
2024-10-21 18:13:46 +0200

    Agregada base de repositorios

---
+++ backend/Server/Server/Repositories/Base/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.Repositories.Base;
+
+public interface IRepository<TEntity, TId> where TEntity : class
+{
+    Task<ICollection<TEntity>> GetAllAsync();
+    IQueryable<TEntity> GetQueryable(bool asNoTracking = true);
+    Task<TEntity> GetByIdAsync(TId id);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> DeleteAsync(TEntity entity);
+    Task<bool> SaveAsync();
+    Task<bool> ExistAsync(TId id);
+}
---
+++ backend/Server/Server/Repositories/Base/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.Repositories.Base;
+
+// TODO: Completar
+
+public abstract class Repository<TEntity, TId> : IRepository<TEntity, TId> where TEntity : class
+{
+    protected readonly FarminhouseContext _context;
+
+    public Repository(FarminhouseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ICollection<TEntity>> GetAllAsync()
+    {
+        return await _context.Set<TEntity>().ToArrayAsync();
+    }
+
+    public async Task<TEntity> GetByIdAsync(TId id)
+    {
+        return await _context.Set<TEntity>().FindAsync(id);
+    }
+
+    public IQueryable<TEntity> GetQueryable(bool asNoTracking = true)
+    {
+        DbSet<TEntity> entities = _context.Set<TEntity>();
+        return asNoTracking ? entities.AsNoTracking() : entities; // "AsNoTracking" permite optimizar la consulta para que no se traten los datos
+    }
+
+    public Task<TEntity> DeleteAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistAsync(TId id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> InsertAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
+
