1c301273d2e4b59119b0e0f331fa2e39b8725c92
AGALMAD <agalmad@g.educaand.es>
2024-10-24 11:09:48 +0200

    Registro de usuario

--- backend/Server/Server/Controllers/AuthController.cs
+++ backend/Server/Server/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Mappers;
+using Server.Models;
 
 namespace Server.Controllers
 {
@@ -7,5 +10,35 @@ namespace Server.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+
+        private readonly UserMapper _userMapper;
+        private readonly UnitOfWork _unitOfWork;
+        FarminhouseContext _context;
+
+        public AuthController(UnitOfWork unitOfWork, UserMapper userMapper, FarminhouseContext context)
+        {
+            _unitOfWork = unitOfWork;
+            _userMapper = userMapper;
+            _context = context;
+        }
+
+
+
+
+        [HttpPost]
+        public async Task RegisterUserAsync
+            (string name, string email, string password, string address)
+        {
+
+            User usuario = new User();
+            usuario.Id = _context.Users.Count() == 0 ? 1 : _context.Users.Max(u => u.Id) + 1;
+            usuario.Name = name;
+            usuario.Email = email;
+            usuario.Password = password;
+            usuario.Address = address;
+            usuario.Role = "normal";
+            await _unitOfWork.UserRepository.InsertAsync(usuario);
+            await _unitOfWork.SaveAsync();
+        }
     }
 }
