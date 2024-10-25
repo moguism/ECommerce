feac4613b89a28e946470e2d93552d3e4753bd40
moguism <mauriciorosso.aboy@gmail.com>
2024-10-25 10:33:38 +0200

    El login y el registro ya funciona correctamente en front y back

---
+++ frontend/src/app/models/login.ts
@@ -0,0 +1,10 @@
+export class Login {
+    Email: string;
+    Password: string;
+
+    public constructor(email: string, password: string)
+    {
+        this.Email = email,
+        this.Password = password
+    }
+}
--- frontend/src/app/services/register.service.ts
+++ frontend/src/app/services/register.service.ts
@@ -11,26 +11,24 @@ import { Result } from '../models/result';
 export class RegisterService {
 
   private BASE_URL = environment.apiUrl;
-
+  jwt : string = ""
 
   constructor(private http: HttpClient) { }
 
-
-  async registerUser<T = void>(path: string, body: Object = {}, contentType = null): Promise<Result<T>> {
+  async registerUser<T = void>(path: string, body: Object = {}): Promise<Result<T>> {
     const url = `${this.BASE_URL}${path}`;
     const request$ = this.http.post(url, body, {
-      headers: this.getHeader(contentType),
-      observe: 'response'
+      headers: this.getHeader(null),
+      observe: 'response',
+      responseType: 'text'
     });
 
     return this.sendRequest<T>(request$);
   }
 
-
-
   private async sendRequest<T = void>(request$: Observable<HttpResponse<any>>): Promise<Result<T>> {
     let result: Result<T>;
-    
+
     try {
       const response = await lastValueFrom(request$);
       const statusCode = response.status;
@@ -46,7 +44,9 @@ export class RegisterService {
       } else {
         result = result = Result.error(statusCode, response.statusText);
       }
+
     } catch (exception : any) {
+      console.log("EXCEPCION: ", exception)
       if (exception instanceof HttpErrorResponse) {
         result = Result.error(exception.status, exception.statusText);
       } else {
@@ -54,10 +54,16 @@ export class RegisterService {
       }
     }
 
+    console.log("RESULT: ", result)
+    if(result.data)
+    {
+      this.jwt = result.data.toString();
+      console.log("HAY MI MADRE EL BICHO: ", this.jwt)
+    }
     return result;
   }
 
-  private getHeader(accept = null, contentType = null): HttpHeaders {
+  private getHeader(accept = null, contentType = "application/json"): HttpHeaders {
     let header: any = {};
 
     // Para cuando haya que poner un JWT
@@ -71,7 +77,4 @@ export class RegisterService {
 
     return new HttpHeaders(header);
   }
-
-
-
 }
