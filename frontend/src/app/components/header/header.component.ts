8d0a70d04d4cd6cd6bbe4992962a678acc907185
FranSiles <francisco.siles.fp@iescampanillas.com>
2024-10-25 12:03:45 +0200

    FINALIZADO EL HEADER NO LO VUELVO A TOCAR(Por ahora)

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { LoginComponent } from '../../pages/login/login.component';
 import { RouterModule } from '@angular/router';
 
@@ -9,6 +9,52 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-
+export class HeaderComponent  {
+  protected cambiodeboton: boolean=true;
+  funcionmostramenu(){
+    this.cambiodeboton=false;
+    const rojo=document.getElementsByClassName("rojo");
+    const textos = document.querySelectorAll(".texto");
+    const negro=document.getElementById("negro");
+    for (let i = 0; i < rojo.length; i++) {
+      rojo[i].className = "rojovisible";
+    }
+    textos.forEach((texto) => {
+      texto.className = "textovisible";
+  });
+    if(negro){
+      negro.id="negrovisible"
+    }
+  }
+  funcioncerrarmenu(){
+    this.cambiodeboton=true;
+    const rojovisible=document.getElementsByClassName("rojovisible");
+    const textosvisibles = document.querySelectorAll(".textovisible");
+    const negrovisible=document.getElementById("negrovisible");
+    const anchoPantalla = window.innerWidth;
+    if(anchoPantalla>400){
+      for (let i = 0; i < rojovisible.length; i++) {
+        rojovisible[i].className = "rojo";
+      }
+      textosvisibles.forEach((textosvisible) => {
+        textosvisible.className = "texto";
+    });
+      if(negrovisible){
+        negrovisible.id="negro"
+      }
+  }
+  for (let i = 0; i < rojovisible.length; i++) {
+    rojovisible[i].className = "rojo";
+  }
+  textosvisibles.forEach((textosvisible) => {
+    textosvisible.className = "texto";
+});
+  if(negrovisible){
+    negrovisible.id="negro"
+  }
+  }
+  @HostListener('window:resize')
+  onResize() {
+    this.funcioncerrarmenu();
+  }
 }
