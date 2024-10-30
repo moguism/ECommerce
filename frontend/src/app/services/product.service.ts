fd28e586282bc632e30f71b9acce930693484e31
AGALMAD <118524298+AGALMAD@users.noreply.github.com>
2024-10-30 20:19:58 +0100

    obtener productos con http

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -9,53 +9,160 @@ import { Observable, forkJoin, lastValueFrom } from 'rxjs';
 })
 export class ProductService {
 
-  readonly BASE_URL = 'https://localhost:7150/api/Product' //URL desde donde se van a recoger todos los productos
+  readonly BASE_URL = 'https://localhost:7150/api/Product/' //URL desde donde se van a recoger todos los productos
 
 
   constructor(private http: HttpClient) { }
 
 
   //Funcion para recoger todos los productos
-  async getAllProducts(): Promise<Product[]> { /*Recoge de forma asíncrona todos los productos desde la BASE_URL y los recoge en un array de producots */
+  async getAllProducts(): Promise<Product[]> {
 
-    const requests: Observable<Object>[] = []; 
+    const requests: Observable<Object>[] = [];
 
 
     requests.push(this.http.get(`${this.BASE_URL}`)); //almacena en el array anterior todos los productos
 
-    /**Espera a que se terminen de hacer todas la peticiones a la URL
-    *Y almacena en el nuevo array todos los objetos de los pokemon ordenados */
+    /*
+     * Espera a que se terminen de hacer todas la peticiones a la URL
+    *  y almacena en el nuevo array todos los objetos ordenados 
+    */
     const allDataRaw: any[] = await lastValueFrom(forkJoin(requests));
 
-    //Array donde se van a almacenar todos los objetos tipo Producto
     const products: Product[] = [];
 
-    //Recorre el array de los productos ordenados
     for (const data of allDataRaw) {
-      //Almacena en esta variable el pokemon con todos sus datos
+
       const product: Product = {
 
         id: data.id,
         name: data.name,
-        description : data.description,
-        price : data.price,
-        stock : data.stock,
-        average : data.average,
-        categoryId : data.categoryId,
-        image_url : data.image_url
+        description: data.description,
+        price: data.price,
+        stock: data.stock,
+        average: data.average,
+        categoryId: data.categoryId,
+        image_url: data.image_url
 
       };
-      //Lo inserta en el array de los pokemon
+      //Lo inserta en el array de los productos
       products.push(product);
 
     }
 
-    //Retorna el array
+    //Retorna el array con todos los productos obtenidos
     return products;
 
 
   }
 
+  //Funcion para recoger todas las verduras
+  async getAllVegetables(): Promise<Product[]> {
+
+    const requests: Observable<Object>[] = [];
+
+
+    requests.push(this.http.get(`${this.BASE_URL}vegetables`)); 
+
+
+    const allDataRaw: any[] = await lastValueFrom(forkJoin(requests));
+
+    const products: Product[] = [];
+
+    for (const data of allDataRaw) {
+
+      const product: Product = {
+
+        id: data.id,
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.stock,
+        average: data.average,
+        categoryId: data.categoryId,
+        image_url: data.image_url
+
+      };
+      products.push(product);
+
+    }
+
+    return products;
+
+
+  }
+
+  //Funcion para recoger todas las frutas
+  async getAllFruits(): Promise<Product[]> {
+
+    const requests: Observable<Object>[] = [];
+
+
+    requests.push(this.http.get(`${this.BASE_URL}fruits`)); 
+
+
+    const allDataRaw: any[] = await lastValueFrom(forkJoin(requests));
+
+    const products: Product[] = [];
+
+    for (const data of allDataRaw) {
+
+      const product: Product = {
+
+        id: data.id,
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.stock,
+        average: data.average,
+        categoryId: data.categoryId,
+        image_url: data.image_url
+
+      };
+      products.push(product);
+
+    }
+
+    return products;
+
+
+  }
+
+  //Funcion para recoger todas las carnes
+  async getAllMeats(): Promise<Product[]> {
+
+    const requests: Observable<Object>[] = [];
+
+
+    requests.push(this.http.get(`${this.BASE_URL}meat`)); 
+
+
+    const allDataRaw: any[] = await lastValueFrom(forkJoin(requests));
+
+    const products: Product[] = [];
+
+    for (const data of allDataRaw) {
+
+      const product: Product = {
+
+        id: data.id,
+        name: data.name,
+        description: data.description,
+        price: data.price,
+        stock: data.stock,
+        average: data.average,
+        categoryId: data.categoryId,
+        image_url: data.image_url
+
+      };
+      products.push(product);
+
+    }
+
+    return products;
+
+
+  }
 
 
 }
