eb0333b217fa022a31c6f359ec4853429d0f049d
FranSiles <91953373+FranSiles@users.noreply.github.com>
2024-10-30 19:29:24 +0100

    Subida del back sin seeder

---
+++ backend/Server/Server/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Mappers;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+        FarminhouseContext _context;
+
+        public ProductController(UnitOfWork unitOfWork, FarminhouseContext context)
+        {
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
+        [HttpGet("vegetables")]
+        public async Task<IEnumerable<Product>> GetAllVegetables()
+        {
+            return await _unitOfWork.ProductRepository.GetAllProductsByCategory("vegetables");
+        }
+        [HttpGet("fruits")]
+        public async Task<IEnumerable<Product>> GetAllFruits()
+        {
+            return await _unitOfWork.ProductRepository.GetAllProductsByCategory("fruits");
+
+        }
+        [HttpGet("meat")]
+        public async Task<IEnumerable<Product>> GetAllMeat()
+        {
+            return await _unitOfWork.ProductRepository.GetAllProductsByCategory("meat");
+
+        }
+    }
+}
--- backend/Server/Server/Repositories/ProductRepository.cs
+++ backend/Server/Server/Repositories/ProductRepository.cs
@@ -1,11 +1,16 @@
-﻿using Server.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
 using Server.Repositories.Base;
 
 namespace Server.Repositories
 {
-    public class ProductRepository : Repository<Order, int>
+    public class ProductRepository : Repository<Product, int>
     {
         public ProductRepository(FarminhouseContext context) : base(context) { }
 
+        public  async Task<ICollection<Product>> GetAllProductsByCategory(string name)
+        {
+            return await GetQueryable().Where(product => product.Category.Name.Equals(name)).ToArrayAsync();
+        }
     }
 }
