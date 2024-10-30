aa9fe5762f8febdc0750ebc64cf408a0b957fb00
moguism <148863935+moguism@users.noreply.github.com>
2024-10-30 16:48:46 +0100

    Continuando trabajo con el about us y corregidas cosas

--- frontend/src/app/pages/about-us/about-us.component.ts
+++ frontend/src/app/pages/about-us/about-us.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  readonly position : number = 1500
+  setUp : boolean = false
 
+  @HostListener('window:scroll', [])
+  checkScroll() {
+    const currentScroll = window.scrollY;
+
+    if(currentScroll >= this.position && !this.setUp)
+    {
+      document.getElementById("test")?.classList.add("test")
+      this.setUp = true;
+    }
+    console.log(currentScroll)
+  }
 }
