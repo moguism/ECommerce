d0e05a80fd284314283b80a0e85af42b88221478
AGALMAD <agalmad@g.educaand.es>
2024-10-30 12:15:37 +0100

    about us creado y enlazado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UserComponent } from './pages/user/user.component';
 import { ProductViewComponent } from './pages/product-view/product-view.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "login", component: LoginComponent},
     {path: "user", component: UserComponent},
-    {path: "product-view", component: ProductViewComponent}
+    {path: "product-view", component: ProductViewComponent},
+    {path: "aboutus", component: AboutusComponent}
+
 ];
