261457c92917ee081aa7628e2e7cc7c3e88e72c2
moguism <148863935+moguism@users.noreply.github.com>
2024-10-31 19:02:00 +0100

    Mejoradas ciertas cosas

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
 import { ApiService } from './api.service';
 import { Result } from '../models/result';
+import { forkJoin, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,36 +13,25 @@ export class ProductService {
   constructor(private api : ApiService) { }
 
 
+  async getProductByName(name: string): Promise<Result<Product[]>>
+  {
+    return this.api.get<Product[]>("Search", name, 'json')
+  }
+  
   async getAllProducts(): Promise<Result<Product[]>> {
-
-
     return this.api.get<Product[]>("Product",{}, 'json');
-    
-
   }
 
   async getAllVegetables(): Promise<Result<Product[]>> {
-
-
-    return this.api.get<Product[]>("Product/vegetables",null,null);
-    
-
+    return this.api.get<Product[]>("Product/vegetables",null,'json');
   }
 
   async getAllFruits(): Promise<Result<Product[]>> {
-
-
-    return this.api.get<Product[]>("Product/fruits",null,null);
-    
-
+    return this.api.get<Product[]>("Product/fruits",null,'json');
   }
 
   async getAllMeats(): Promise<Result<Product[]>> {
-
-
-    return this.api.get<Product[]>("Product/meat",null,null);
-    
-
+    return this.api.get<Product[]>("Product/meat",null,'json');
   }
 
 }
