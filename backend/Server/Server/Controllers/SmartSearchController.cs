83ac452edfab19bb28126b04056c41bd4c643ce3
moguism <148863935+moguism@users.noreply.github.com>
2024-10-31 18:05:50 +0100

    Agregado ProductMapper y Búsqueda Inteligente

---
+++ backend/Server/Server/Controllers/SmartSearchController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.Services;
+
+namespace Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SmartSearchController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<string> Search([FromQuery] string query)
+    {
+        SmartSearchService smartSearchService = new SmartSearchService();
+        return smartSearchService.Search(query);
+    }
+}
