49deab7744ff961a0fee174c1396808981ba4c12
AGALMAD <agalmad@g.educaand.es>
2024-11-01 10:02:21 +0100

    objeto query en front

--- frontend/src/app/models/query.ts
+++ frontend/src/app/models/query.ts
@@ -1,10 +1,26 @@
-import { OrdinationDirection } from "./enums/ordination-direction";
-import { OrdinationType } from "./enums/ordination-type";
-import { ProductType } from "./enums/product-type";
+import { OrdinationDirection } from "./enums/ordination-direction"
+import { OrdinationType } from "./enums/ordination-type"
+import { ProductType } from "./enums/product-type"
 
-export interface Query {
-    ordination_direction : OrdinationDirection
-    ordination_type: OrdinationType
-    product_type: ProductType
+export class Query {
+    productType: ProductType;
+    ordinationType: OrdinationType;
+    ordinationDirection : OrdinationDirection;
+    productPageName : number;
+    productPageSize : number;
+    actualPage : number;
 
+
+
+    public constructor(productType : ProductType, ordinationType : OrdinationType, ordinationDirection : OrdinationDirection,  
+        productPageName : number , productPageSize : number, actualPage : number )
+    {
+
+        this.productType = productType;
+        this.ordinationType = ordinationType;
+        this.ordinationDirection = ordinationDirection;
+        this.productPageName = productPageName;
+        this.productPageSize = productPageSize;
+        this.actualPage = actualPage;
+    }
 }
