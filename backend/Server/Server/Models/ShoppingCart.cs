315959a51310d03d797edfaa39fc88a14cc8363c
AGALMAD <118524298+AGALMAD@users.noreply.github.com>
2024-11-04 18:00:31 +0100

    obtener productos del carrito desde controlador

--- backend/Server/Server/Models/ShoppingCart.cs
+++ backend/Server/Server/Models/ShoppingCart.cs
@@ -2,5 +2,10 @@
 {
     public class ShoppingCart
     {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
     }
 }
