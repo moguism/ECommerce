e8d863d6291aa1f17892e00a6ced68f6ee171719
dcuecal515 <dcuecal515@g.educaand.es>
2024-11-05 11:27:54 +0100

    Hecho pdf de ML.NET

---
+++ backend/Server/Server/Models/ModelInput.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace Server.Models
+{
+    public class ModelInput
+    {
+        [LoadColumn(0)]
+        [ColumnName(@"text")]
+        public string  Text { get; set; }
+
+        [LoadColumn(1)]
+        [ColumnName(@"label")]
+        public float Label { get; set; }
+    }
+}
--- backend/Server/Server/Program.cs
+++ backend/Server/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.ML;
 using Microsoft.IdentityModel.Tokens;
 using Server.Mappers;
 using Server.Models;
@@ -63,6 +64,10 @@ namespace Server
                         })
                     );
             }
+
+            // Para el modelo de IA
+            builder.Services.AddPredictionEnginePool<ModelInput, ModelOutput>()
+                .FromFile("IAFarminhouse.mlnet");
 
             var app = builder.Build();
 
