b388caa3012803c85d2690d534bb67d07455ffdb
moguism <148863935+moguism@users.noreply.github.com>
2024-11-06 15:57:57 +0100

    Cositas

--- backend/Server/Server/Models/CartContent.cs
+++ backend/Server/Server/Models/CartContent.cs
@@ -6,5 +6,7 @@
         public int Id { get; set; }
         public int ProductId { get; set; }
         public int ShoppingCartId { get; set; }
+
+        public Product Product { get; set; }
     }
 }
