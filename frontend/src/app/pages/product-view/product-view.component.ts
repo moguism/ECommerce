0c1698b06d8cc22fdfa1a5259ed01fea6e40c1a2
AGALMAD <118524298+AGALMAD@users.noreply.github.com>
2024-11-07 21:12:36 +0100

    cambios

--- frontend/src/app/pages/product-view/product-view.component.ts
+++ frontend/src/app/pages/product-view/product-view.component.ts
@@ -6,6 +6,7 @@ import { ProductService } from '../../services/product.service';
 import { ApiService } from '../../services/api.service';
 import { CartContent } from '../../models/cart-content';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-product-view',
   standalone: true,
@@ -15,55 +16,58 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProductViewComponent implements OnInit {
 
-  protected count=0;
-  product: Product | null = null
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private apiService: ApiService){}
+  protected count = 0;
+  product: Product | null = null;
+  routeParamMap$: Subscription | null = null;
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private apiService: ApiService) { }
 
-  ngOnInit(): void
-  {
+  ngOnInit(): void {
     const id = this.activatedRoute.snapshot.paramMap.get('id') as unknown as number;
     this.getProduct(id)
+
   }
 
-  async getProduct(id: number)
-  {
-    const result = await this.productService.getById(id)
-    if(result != null)
-    {
-      this.product = result
-    }
+  async getProduct(id: number) {
+
+    this.routeParamMap$ = this.activatedRoute.paramMap.subscribe(async paramMap => {
+      const result = await this.productService.getById(id)
+      if (result != null) {
+        this.product = result
+      }
+    });
+
   }
 
-  sumar(){
+  sumar() {
     this.count++;
   }
-  restar(){
-    if(this.count>0){
+  restar() {
+    if (this.count > 0) {
       this.count--
     }
   }
 
-  
-  async addToCart(product: Product)
-  {
+
+  async addToCart(product: Product) {
     const productsLocalStore = localStorage.getItem("shoppingCart")
-    if(this.apiService.jwt == "")
-    {
-        let allProducts : Product[] = []
-        const productsLocalStore = localStorage.getItem("shoppingCart")
-        if(productsLocalStore)
-        {
-          allProducts = JSON.parse(productsLocalStore)
-          allProducts.push(product)
-          localStorage.setItem("shoppingCart", JSON.stringify(allProducts))
-        }
+    if (this.apiService.jwt == "") {
+      let allProducts: Product[] = []
+      const productsLocalStore = localStorage.getItem("shoppingCart")
+      if (productsLocalStore) {
+        allProducts = JSON.parse(productsLocalStore)
+        allProducts.push(product)
+        localStorage.setItem("shoppingCart", JSON.stringify(allProducts))
+      }
     }
-    else
-    {
+    else {
       localStorage.removeItem("shoppingCart")
       const cartContent = new CartContent(1, product.id)
       await this.apiService.post("ShoppingCart", cartContent)
     }
   }
 
+  ngOnDestroy(): void {
+    this.routeParamMap$?.unsubscribe();
+
+  }
 }
