f70cc19174f598f634116097a9f77c4a2bbcd73c
moguism <148863935+moguism@users.noreply.github.com>
2024-11-11 17:45:42 +0100

    Fallo solucionado

---
+++ backend/Server/Server/Controllers/TemporalOrderController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.DTOs;
+using Server.Mappers;
+using Server.Models;
+using Server.Repositories;
+using Server.Services;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemporalOrderController : ControllerBase
+    {
+
+        private readonly ShoppingCartMapper _shoppingCartMapper;
+        private readonly ShoppingCartService _shoppingCartService;
+        private readonly UnitOfWork _unitOfWork;
+
+        public TemporalOrderController(ShoppingCartMapper shoppingCartMapper, ShoppingCartService shoppingCartService, UnitOfWork unitOfWork)
+        {
+            _shoppingCartMapper = shoppingCartMapper;
+            _shoppingCartService = shoppingCartService;
+            _unitOfWork = unitOfWork;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task CreateTemporalOrder()
+        {
+
+            User user = await GetAuthorizedUser();
+            if (user == null)
+            {
+                return;
+            }
+
+            ShoppingCart cart = await _unitOfWork.ShoppingCartRepository.GetAllByUserIdAsync(user.Id);
+        }
+
+        private async Task<User> GetAuthorizedUser()
+        {
+            // Pilla el usuario autenticado según ASP
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            string idString = currentUser.Claims.First().ToString().Substring(3); // 3 porque en las propiedades sale "id: X", y la X sale en la tercera posición
+
+            // Pilla el usuario de la base de datos
+            return await _shoppingCartService.GetUserFromDbByStringId(idString);
+        }
+
+    }
+}
