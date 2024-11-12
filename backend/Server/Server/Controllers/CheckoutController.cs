9ab21389fae03a235e9c518a26d28ad1e027a284
AGALMAD <agalmad@g.educaand.es>
2024-11-12 13:35:43 +0100

    creación del controlador del pago

---
+++ backend/Server/Server/Controllers/CheckoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+
+        private readonly Settings _settings;
+
+        public CheckoutController(Settings settings)
+        {
+            _settings = settings;
+        }
+
+
+
+
+
+
+    }
+}
