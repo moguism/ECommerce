91427438d8af7b864c7e38990775e1914c60a487
dcuecal515 <dcuecal515@g.educaand.es>
2024-11-14 17:10:01 +0100

    CleanTemporalOrderService arreglado

--- backend/Server/Server/Services/CleanTemporalOrdersService.cs
+++ backend/Server/Server/Services/CleanTemporalOrdersService.cs
@@ -5,41 +5,51 @@ namespace Server.Services;
 public class CleanTemporalOrdersService : BackgroundService
 {
     private readonly TimeSpan _cleanupInterval = TimeSpan.FromMinutes(5);
+    private readonly IServiceProvider _serviceProvider;
 
-    /*public CleanTemporalOrdersService(TemporalOrderService temporalOrderService)
+    public CleanTemporalOrdersService(IServiceProvider serviceProvider)
     {
-        _temporalOrderService = temporalOrderService;
-    }*/
+        _serviceProvider = serviceProvider;
+    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        /*FarminhouseContext farminhouseContext = new FarminhouseContext();
-        UnitOfWork unitOfWork = new UnitOfWork(farminhouseContext);
         while (!stoppingToken.IsCancellationRequested)
         {
-            Console.WriteLine("Ejecutando servicio en segundo plano");
-            List<TemporalOrder> expiredOrders = (List<TemporalOrder>) await unitOfWork.TemporalOrderRepository.GetExpiredOrders(DateTime.UtcNow);
-
-            foreach (TemporalOrder temporalOrder in expiredOrders)
+            using (var scope = _serviceProvider.CreateScope())
             {
-                unitOfWork.TemporalOrderRepository.Delete(temporalOrder);
-                
-                ShoppingCart cart = await unitOfWork.ShoppingCartRepository.GetFullByIdAsync(temporalOrder.ShoppingCartId);
-                cart.Temporal = false;
-                unitOfWork.ShoppingCartRepository.Update(cart);
+                var unitOfWork = scope.ServiceProvider.GetService<UnitOfWork>();
+                try {
+                    Console.WriteLine("Ejecutando servicio en segundo plano");
+                    List<TemporalOrder> expiredOrders = (List<TemporalOrder>)await unitOfWork.TemporalOrderRepository.GetExpiredOrders(DateTime.UtcNow);
 
-                List<CartContent> cartContents = (List<CartContent>)cart.CartContent;
-                foreach (CartContent cartContent in cartContents)
+                    foreach (TemporalOrder temporalOrder in expiredOrders)
+                    {
+                        unitOfWork.TemporalOrderRepository.Delete(temporalOrder);
+
+                        ShoppingCart cart = await unitOfWork.ShoppingCartRepository.GetFullByIdAsync(temporalOrder.ShoppingCartId);
+                        cart.Temporal = false;
+                        unitOfWork.ShoppingCartRepository.Update(cart);
+
+                        List<CartContent> cartContents = (List<CartContent>)cart.CartContent;
+                        foreach (CartContent cartContent in cartContents)
+                        {
+                            Product product = await unitOfWork.ProductRepository.GetFullProductById(cartContent.ProductId);
+                            product.Stock += cartContent.Quantity;
+                            unitOfWork.ProductRepository.Update(product);
+                        }
+                    }
+
+                    await unitOfWork.SaveAsync();
+                } catch(Exception e)
                 {
-                    Product product = await unitOfWork.ProductRepository.GetFullProductById(cartContent.ProductId);
-                    product.Stock += cartContent.Quantity;
-                    unitOfWork.ProductRepository.Update(product);
+                    Console.WriteLine($"Error durante la ejecución del servicio: {e.Message}");
                 }
+                
             }
-
-            await unitOfWork.SaveAsync();
+            
 
             await Task.Delay(_cleanupInterval, stoppingToken);
-        }*/
+        }
     }
 }
