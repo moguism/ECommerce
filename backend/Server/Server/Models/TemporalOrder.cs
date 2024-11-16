c6923f59932973532c045ce5a72a8672cd007483
AGALMAD <agalmad@g.educaand.es>
2024-11-16 12:19:04 +0100

    cambio en el diagrama de la base de datos y en el modelo de orden temporal

--- backend/Server/Server/Models/TemporalOrder.cs
+++ backend/Server/Server/Models/TemporalOrder.cs
@@ -1,17 +1,18 @@
-﻿namespace Server.Models
+﻿using Server.DTOs;
+
+namespace Server.Models
 {
     public class TemporalOrder
     {
         public int Id { get; set; }
-        public int ShoppingCartId { get; set; }
-        //public int UserId { get; set; }
-        public int PaymentTypeId { get; set; }
-        public PaymentsType PaymentsType { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
 
-        //public User User { get; set; }
-        public ShoppingCart ShoppingCart { get; set; }
-        public ICollection<Order> Orders { get; set; } = new List<Order>();
-        public DateTime ExpirationDate { get; set; }
-        public bool Finished { get; set; }
+        /*Lista de productos y cantidad para poder almacenar tanto el carrito del usuario autenticado
+         * Como el carrito almacenado en el Local Storage si no tiene sesión iniciada antes del pago
+         * 
+         */
+        public IEnumerable<CartContentDto> CartContent { get; set; } = new List<CartContentDto>();
+
     }
 }
