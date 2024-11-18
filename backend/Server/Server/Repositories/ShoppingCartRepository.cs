5521148a5e319a1245b3ff04c227841053fb24d7
dcuecal515 <163828595+dcuecal515@users.noreply.github.com>
2024-11-18 17:17:34 +0100

    Ya se elimina el carro al pagar correctamente

--- backend/Server/Server/Repositories/ShoppingCartRepository.cs
+++ backend/Server/Server/Repositories/ShoppingCartRepository.cs
@@ -80,7 +80,11 @@ namespace Server.Repositories
             return shoppingCart;
         }
 
-
+        public async Task DeleteShoppingCartByShoppingCartIdAsync(int shoppingCartId)
+        {
+            ShoppingCart shoppingCart = await _context.ShoppingCart.FirstOrDefaultAsync(c => c.Id == shoppingCartId);
+            _context.ShoppingCart.Remove(shoppingCart);
+        }
 
     }
 }
