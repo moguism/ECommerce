174396842a650b8f2b8b09845c70751f49070dd9
AGALMAD <agalmad@g.educaand.es>
2024-12-08 13:25:32 +0100

    cambios al servicio del carrito

--- frontend/src/app/pages/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -19,77 +19,26 @@ import { QuantityModifierComponent } from '../../components/quantity-modifier/qu
   styleUrl: './shopping-cart.component.css'
 })
 export class ShoppingCartComponent implements OnInit {
-  shoppingCartProducts: Product[] = []
   productsToBuy: CartContent[] = [];
 
 
-  constructor(private productService: ProductService, private apiService: ApiService, private router: Router, private shoppingCartService: ShoppingCartService) { }
+  constructor(private productService: ProductService, private apiService: ApiService, private router: Router, 
+    public shoppingCartService: ShoppingCartService) { }
 
   async ngOnInit(): Promise<void> {
     
     const goToCheckout = localStorage.getItem("goToCheckout")
-    console.log("yctgvtuhbijnokl  --- " + goToCheckout)
     if (this.apiService.jwt != "" && goToCheckout && goToCheckout == "true") {
       await this.createDirectPayment();
       return
     }
     else {
-      this.getShoppingCart();
+      this.shoppingCartService.getShoppingCart();
     }
       
   }
 
-  getLocalStorageCart() {
-    //this.shoppingCartProducts = [];
-    const productsRaw = localStorage.getItem("shoppingCart");
-    if (productsRaw) {
-      this.shoppingCartProducts = JSON.parse(productsRaw);
-    }
-  }
-
-
-  async getShoppingCart() {
-    //this.shoppingCartService.getShoppingCartCount()
-    this.shoppingCartProducts = [];
-
-    /*if (this.apiService.jwt !== "" && this.shoppingCartProducts.length > 0) {
-      console.log("Sincronizando productos locales al carrito del backend...");
-
-      for (const product of this.shoppingCartProducts) {
-        const cartContent = new CartContent(product.id, product.total);
-        await this.apiService.post("ShoppingCart/addProductOrChangeQuantity", cartContent);
-      }
-
-      localStorage.removeItem("shoppingCart");
-      
-    }*/
-
-    // Podríamos optimizar esto haciendo que en el login ponga el contenido del carrito en el localStorage, de manera que no tenga que hacer peticiones
-    // Sin embargo, si un admin borra o cambia un producto, cagamos
-    if (this.apiService.jwt !== "") {
-      const result = await this.apiService.get("ShoppingCart", {}, 'json');
-      if (result.data) {
-        const data: any = result.data;
-        const cartContent: any[] = data.cartContent;
-        for (const product of cartContent) {
-          let p = product.product
-          p.total = product.quantity
-          p = this.shoppingCartService.addCorrectPath(p)
-          this.shoppingCartProducts.push(p);
-        }
-      }
-      console.log("CARRITO SINCRONIZADO: ", this.shoppingCartProducts);
-
-      
-    }
-    else {
-      this.getLocalStorageCart();
-    }
-
-    
-    this.shoppingCartService.contProduct = this.shoppingCartProducts.length
-
-  }
+  
 
   async changeQuantity(product: Product) {
     const input = document.getElementById(product.id.toString()) as HTMLInputElement
@@ -105,67 +54,41 @@ export class ShoppingCartComponent implements OnInit {
 
     if (input) {
       if (this.apiService.jwt == "") {
-        const p = this.findProductInArray(product.id)
+        const p = this.shoppingCartService.findProductInArray(product.id)
         p.total = parseInt(input.value);
-        localStorage.setItem("shoppingCart", JSON.stringify(this.shoppingCartProducts));
+        localStorage.setItem("shoppingCart", JSON.stringify(this.shoppingCartService.shoppingCartProducts));
       }
       else {
         localStorage.removeItem("shoppingCart")
 
         const cartContent = new CartContent(product.id, parseInt(input.value), product)
         await this.apiService.post("ShoppingCart/addProductOrChangeQuantity", cartContent)
-        this.getShoppingCart()
+        this.shoppingCartService.getLocalStorageCart()
       }
     }
   }
 
-  async deleteProduct(productId: number) {
-    const index = this.shoppingCartProducts.findIndex(product => product.id === productId);
+  
 
-    if (index !== -1) {
-      const product = this.shoppingCartProducts[index];
-
-      if (product.total > 1) {
-        product.total -= 1;
-      } else {
-        this.shoppingCartProducts.splice(index, 1);
-      }
-
-      if (this.apiService.jwt == "") {
-        this.deleteFromArray(product, false)
-      }
-      else {
-        await this.apiService.delete("ShoppingCart", { productId })
-        this.getShoppingCart()
-      }
-    }
-
-  }
-
-  findProductInArray(id: number): Product {
-    const index = this.shoppingCartProducts.findIndex(product => product.id === id);
-    const product = this.shoppingCartProducts[index];
-    return product;
-  }
 
   async pay(method: string) {
 
 
-    if (this.shoppingCartProducts.length == 0) {
+    if (this.shoppingCartService.shoppingCartProducts.length == 0) {
       alert("No hay nada que pagar, bobolón")
       return
     }
-    for (const product of this.shoppingCartProducts) {
+    for (const product of this.shoppingCartService.shoppingCartProducts) {
       const newProduct = await this.productService.getById(product.id)
       if (newProduct) {
         const difference = newProduct.stock - product.stock;
         if (difference < 0) {
-          this.deleteFromArray(product, true)
+          this.shoppingCartService.deleteFromArray(product, true)
           return
         }
       }
       else {
-        this.deleteFromArray(product, true)
+        this.shoppingCartService.deleteFromArray(product, true)
         return
       }
 
@@ -195,9 +118,9 @@ export class ShoppingCartComponent implements OnInit {
 
   // Esta función solo debería servir para el carrito local
   async createDirectPayment() {
-    this.getLocalStorageCart()
+    this.shoppingCartService.getLocalStorageCart()
     const cartContents: CartContent[] = []
-    for (const product of this.shoppingCartProducts) {
+    for (const product of this.shoppingCartService.shoppingCartProducts) {
       const cartContent = new CartContent(product.id, product.total, product)
       cartContents.push(cartContent)
     }
@@ -219,22 +142,17 @@ export class ShoppingCartComponent implements OnInit {
     }
   }
 
-  deleteFromArray(product: Product, showAlert: boolean) {
-    const index = this.shoppingCartProducts.findIndex(p => p.id === product.id);
-    this.shoppingCartProducts.splice(index, 1);
-    localStorage.setItem("shoppingCart", JSON.stringify(this.shoppingCartProducts))
-    if (showAlert) {
-      alert("Uno o varios productos han sido eliminados por falta de stock")
-    }
-  }
 
   totalprice() {
     let totalcount = 0;
-    for (const product of this.shoppingCartProducts) {
+    for (const product of this.shoppingCartService.shoppingCartProducts) {
       totalcount += product.total * product.price;
     }
     return totalcount;
   }
+
+  /*
+
   sumar(index: number) {
     const quantity = this.shoppingCartProducts.findIndex(product => product.id === index);
     this.shoppingCartProducts[quantity].total++;
@@ -245,6 +163,7 @@ export class ShoppingCartComponent implements OnInit {
       this.shoppingCartProducts[quantity].total--;
     }
   }
+    */
 
 
 
--- frontend/src/app/services/shopping-cart.service.ts
+++ frontend/src/app/services/shopping-cart.service.ts
@@ -10,14 +10,105 @@ export class ShoppingCartService {
 
   total: number = 0
   contProduct : number = 0
-  constructor(private api: ApiService) { }
+  shoppingCartProducts: Product[] = []
+
+
+
+  constructor(private apiService: ApiService) { }
+
+
+  getLocalStorageCart() {
+    //this.shoppingCartProducts = [];
+    const productsRaw = localStorage.getItem("shoppingCart");
+    if (productsRaw) {
+      this.shoppingCartProducts = JSON.parse(productsRaw);
+    }
+  }
+
+
+  async getShoppingCart() {
+
+    this.shoppingCartProducts = [];
+
+   
+
+    // Podríamos optimizar esto haciendo que en el login ponga el contenido del carrito en el localStorage, de manera que no tenga que hacer peticiones
+    // Sin embargo, si un admin borra o cambia un producto, cagamos
+    if (this.apiService.jwt !== "") {
+      const result = await this.apiService.get("ShoppingCart", {}, 'json');
+      if (result.data) {
+        const data: any = result.data;
+        const cartContent: any[] = data.cartContent;
+        for (const product of cartContent) {
+          let p = product.product
+          p.total = product.quantity
+          p = this.addCorrectPath(p)
+          this.shoppingCartProducts.push(p);
+        }
+      }
+      console.log("CARRITO SINCRONIZADO: ", this.shoppingCartProducts);
+
+      
+    }
+    else {
+      this.getLocalStorageCart();
+    }
+
+    
+    this.contProduct = this.shoppingCartProducts.length
+
+  }
+
+
+  async deleteProduct(productId: number) {
+    const index = this.shoppingCartProducts.findIndex(product => product.id === productId);
+
+    if (index !== -1) {
+      const product = this.shoppingCartProducts[index];
+
+      if (product.total > 1) {
+        product.total -= 1;
+      } else {
+        this.shoppingCartProducts.splice(index, 1);
+      }
+
+      if (this.apiService.jwt == "") {
+        this.deleteFromArray(product, false)
+      }
+      else {
+        await this.apiService.delete("ShoppingCart", { productId })
+        this.getShoppingCart()
+      }
+    }
+
+  }
+
+
+  findProductInArray(id: number): Product {
+    const index = this.shoppingCartProducts.findIndex(product => product.id === id);
+    const product = this.shoppingCartProducts[index];
+    return product;
+  }
+
+
+  deleteFromArray(product: Product, showAlert: boolean) {
+    const index = this.shoppingCartProducts.findIndex(p => p.id === product.id);
+    this.shoppingCartProducts.splice(index, 1);
+    localStorage.setItem("shoppingCart", JSON.stringify(this.shoppingCartProducts))
+    if (showAlert) {
+      alert("Uno o varios productos han sido eliminados por falta de stock")
+    }
+  }
+
+
+
 
   async getShoppingCartCount()
   {
     // Si quisiésemos podríamos recorrer el Array e ir sumando las cantidades con un for, ya como veáis
-    if(this.api.jwt != "" && this.api.jwt != null)
+    if(this.apiService.jwt != "" && this.apiService.jwt != null)
       {
-        const result = await this.api.get("ShoppingCart", {}, 'json');
+        const result = await this.apiService.get("ShoppingCart", {}, 'json');
         if(result.data)
         {
           const data: any = result.data;
